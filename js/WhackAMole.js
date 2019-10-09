29c90de9556c6ebe9a94ad0a500a38b444adfb15
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-10-09 15:50:16 -0500

    WIP whack-a-mole

---
+++ js/WhackAMole.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class WhackAMole{
+    constructor(startButton, moles, scoreOut, gameTimeLength, peepTimeMin, peepTimeMax){
+        this.btnStart = startButton;
+        this.moles = moles;
+        this.scoreOut = scoreOut;
+
+        this.moleImgSrc = "../img/mole.png";
+        this.moleBankedImg = new Image();
+        this.molebonkedImg.src =
+    }
+}
